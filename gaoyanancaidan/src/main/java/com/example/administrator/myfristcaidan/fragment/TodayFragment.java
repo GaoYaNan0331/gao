b5110da798c61e619e772583a638d82b92904d03
gaoyanan0331 <1105142987@qq.com>
2017-04-09 14:07:12 +0800

    侧滑菜单

---
+++ gaoyanancaidan/src/main/java/com/example/administrator/myfristcaidan/fragment/TodayFragment.java
@@ -0,0 +1,43 @@
+package com.example.administrator.myfristcaidan.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.myfristcaidan.R;
+
+
+public class TodayFragment extends Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.frag_today, null);
+		return view;
+	}
+	
+	
+	
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+	}
+	
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+}
